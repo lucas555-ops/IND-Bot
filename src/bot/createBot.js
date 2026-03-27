375d713556145da7dbcb8274ef83b0bdcc5b0608
lucas555-ops <lucas9589716@gmail.com>
2026-03-27 12:49:37 +0500

    Create createBot.js

---
+++ src/bot/createBot.js
@@ -0,0 +1,71 @@
+import { Bot } from 'grammy';
+import { getAppConfig, getTelegramConfig } from '../config/env.js';
+import { createDirectoryComposer } from './composers/directoryComposer.js';
+import { createHomeComposer } from './composers/homeComposer.js';
+import { createIntroComposer } from './composers/introComposer.js';
+import { createOperatorComposer } from './composers/operatorComposer.js';
+import { createProfileComposer } from './composers/profileComposer.js';
+import { createTextComposer } from './composers/textComposer.js';
+import { createSurfaceBuilders } from './surfaces/appSurfaces.js';
+import { formatIntroDecisionReason, formatIntroRequestReason } from './utils/notices.js';
+import { clearAllPendingInputs } from './utils/pendingInputs.js';
+
+let botSingleton = null;
+
+export function createBot() {
+  if (botSingleton) {
+    return botSingleton;
+  }
+
+  const { botToken } = getTelegramConfig();
+  const { appBaseUrl } = getAppConfig();
+  const bot = new Bot(botToken);
+
+  const surfaces = createSurfaceBuilders({ appBaseUrl });
+
+  bot.use(createHomeComposer({
+    appBaseUrl,
+    clearAllPendingInputs,
+    buildHomeSurface: surfaces.buildHomeSurface
+  }));
+
+  bot.use(createIntroComposer({
+    clearAllPendingInputs,
+    buildIntroInboxSurface: surfaces.buildIntroInboxSurface,
+    buildIntroDetailSurface: surfaces.buildIntroDetailSurface,
+    buildDirectoryCardSurface: surfaces.buildDirectoryCardSurface,
+    formatIntroDecisionReason
+  }));
+
+  bot.use(createProfileComposer({
+    clearAllPendingInputs,
+    buildProfileMenuSurface: surfaces.buildProfileMenuSurface,
+    buildProfilePreviewSurface: surfaces.buildProfilePreviewSurface,
+    buildProfileSkillsSurface: surfaces.buildProfileSkillsSurface
+  }));
+
+  bot.use(createDirectoryComposer({
+    clearAllPendingInputs,
+    buildDirectoryListSurface: surfaces.buildDirectoryListSurface,
+    buildDirectoryCardSurface: surfaces.buildDirectoryCardSurface,
+    buildDirectoryFiltersSurface: surfaces.buildDirectoryFiltersSurface,
+    buildIntroInboxSurface: surfaces.buildIntroInboxSurface,
+    formatIntroRequestReason
+  }));
+
+  bot.use(createTextComposer({
+    buildDirectoryFiltersSurface: surfaces.buildDirectoryFiltersSurface
+  }));
+
+  bot.use(createOperatorComposer({
+    clearAllPendingInputs,
+    buildOperatorDiagnosticsSurface: surfaces.buildOperatorDiagnosticsSurface
+  }));
+
+  bot.catch((error) => {
+    console.error('[bot.catch]', error.error);
+  });
+
+  botSingleton = bot;
+  return botSingleton;
+}
